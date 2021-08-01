9c4e9425f5141c6ad9df5b7de5ac3db860a5bb10
jperalgo <jperalgo@sfu.ca>
2021-07-31 19:36:27 -0700

    Encoded Data Cross_val Score is negative (I don't know what to make of it)

--- test/main.py
+++ test/main.py
@@ -7,15 +7,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
 import pandas as pd
 
 studentMath = pd.read_csv('student-mat.csv', ';')
-print(studentMath.shape)
-print(studentMath.isna().sum())
+# print(studentMath.shape)
+# print(studentMath.isna().sum())
 
 ###
 # LINEAR REGRESSION MODEL
-# Ignore 2, 4, 5, 11, 12, 20, 23, G1, G2 and keeps the other rows
+# Ignore 1, 2, 4, 5, 11, 12, 20, 23, G1, G2 and keeps the other rows
 #
 studentMath = studentMath.loc[:, ['school', 'age', 'Pstatus', 'Medu', 'Fedu',
        'Mjob', 'Fjob', 'traveltime', 'studytime',
@@ -23,36 +25,48 @@ studentMath = studentMath.loc[:, ['school', 'age', 'Pstatus', 'Medu', 'Fedu',
        'higher', 'internet', 'famrel', 'freetime', 'goout', 'Dalc',
        'Walc', 'health', 'absences', 'G3']]
 
-x = studentMath.loc[:, ['school', 'age', 'Pstatus', 'Medu', 'Fedu',
+x = studentMath.loc[:, ['age', 'Medu', 'Fedu',
        'Mjob', 'Fjob', 'traveltime', 'studytime',
        'failures', 'schoolsup', 'famsup', 'paid', 'activities',
        'higher', 'internet', 'famrel', 'freetime', 'goout', 'Dalc',
        'Walc', 'health', 'absences']]
 
-x = studentMath.loc[:, ['studytime']]
 y = studentMath.G3
+# x = studentMath.loc[:, ['studytime']]
 
-print(x.shape)
+# print(x.shape)
 print(x.head)
-print(y.shape)
+# print(y.shape)
+
+#ONE HOT ENCODER
+ohe = OneHotEncoder(sparse=False)
+# print(ohe.fit_transform(x[['Pstatus']]))
+# print(ohe.categories_)
+
+columnTrans = make_column_transformer((OneHotEncoder(),
+                                      ['Mjob', 'Fjob', 'schoolsup', 'famsup', 'paid', 'activities',
+                                       'higher', 'internet']), remainder='passthrough')
+print("columnTransform: ", columnTrans.fit_transform(x))
 
 #Creating Linear Regression Model
 linReg = LinearRegression()
-linReg.fit(x, y)
-
-print("CVS:", cross_val_score(linReg, x, y, cv=3).mean())
-print(y.value_counts(normalize=True))
-
-yPred = linReg.predict(x)
-
-print('Coefficients: \n', linReg.coef_)
-print('Mean squared error: %.2f' % mean_squared_error(y, yPred))
-print('Coefficient of determination: %.2f' % r2_score(y, yPred))
+pipeLine = make_pipeline(columnTrans, linReg)
+print(cross_val_score(pipeLine, x, y, cv=3).mean())
 
 
-plt.scatter(x, y, edgecolors='red')
-plt.plot(x, yPred, 'o', color='black')
-plt.xticks(())
-plt.yticks(())
-
-plt.show()
+# print("CVS:", cross_val_score(linReg, x, y, cv=3).mean())
+# print(y.value_counts(normalize=True))
+#
+# yPred = linReg.predict(x)
+#
+# print('Coefficients: \n', linReg.coef_)
+# print('Mean squared error: %.2f' % mean_squared_error(y, yPred))
+# print('Coefficient of determination: %.2f' % r2_score(y, yPred))
+#
+#
+# plt.scatter(x, y, edgecolors='red')
+# plt.plot(x, yPred, 'o', color='black')
+# plt.xticks(())
+# plt.yticks(())
+#
+# plt.show()
